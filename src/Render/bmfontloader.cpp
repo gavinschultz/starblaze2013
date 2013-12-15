5443d748c8f3c9fc946538c635730b98955624aa
Gavin Schultz-Ohkubo <gavin.schultz.ohkubo@gmail.com>
2013-12-15 09:09:44 +0900

    Back to working status for font. Nothing Ive done with bitmap fonts has signficantly helped performance. :)

--- src/Render/bmfontloader.cpp
+++ src/Render/bmfontloader.cpp
@@ -13,8 +13,8 @@ namespace
 {
 	SDL_Texture* texture_;
 	SDL_Rect texture_rect_;
-	std::unique_ptr<bmfont::BMFontFile> bmfile_;
-	std::unique_ptr<std::unordered_map<int, MappedTexture>> mapped_textures_;
+	bmfont::BMFontFile bmfile_;
+	std::unordered_map<int, MappedTexture> mapped_textures_;
 	bmfont::BMFontLine parseLine(const std::string& line)
 	{
 		std::stringstream ss{ line };
@@ -122,40 +122,6 @@ namespace
 	}
 }
 
-//bmfont::BMFontFile::BMFontFile() = default;
-//bmfont::BMFontFile::~BMFontFile() = default;
-//bmfont::BMFontFile::BMFontFile(const bmfont::BMFontFile& rhs) : chars(rhs.chars), info(rhs.info), page(rhs.page)
-//{
-//	debug::console({ "BMFontFile copy constructor called..." });
-//}
-//bmfont::BMFontFile& bmfont::BMFontFile::operator=(const bmfont::BMFontFile& rhs)
-//{
-//	if (this != &rhs)
-//	{
-//		debug::console({ "BMFontFile copy assignment constructor called..." });
-//		this->chars = rhs.chars;
-//		this->info = rhs.info;
-//		this->page = rhs.page;
-//	}
-//	return *this;
-//}
-//
-//bmfont::BMFontFile::BMFontFile(bmfont::BMFontFile&& rhs)
-//{
-//	debug::console({ "BMFontFile move constructor called..." });
-//	this->chars = std::move(rhs.chars);
-//	this->info = std::move(rhs.info);
-//	this->page = std::move(rhs.page);
-//}
-//bmfont::BMFontFile& bmfont::BMFontFile::operator=(bmfont::BMFontFile&& rhs)
-//{
-//	debug::console({ "BMFontFile move assignment constructor called..." });
-//	this->chars = std::move(rhs.chars);
-//	this->info = std::move(rhs.info);
-//	this->page = std::move(rhs.page);
-//	return *this;
-//}
-
 void bmfont::load(SDL_Renderer* sdl_renderer, const std::string& bmfont_path)
 {
 	debug::console({ "Loading mapped textures from BMFont file in ", bmfont_path });
@@ -176,39 +142,28 @@ void bmfont::load(SDL_Renderer* sdl_renderer, const std::string& bmfont_path)
 		program::exit(RetCode::runtime_error, { e.what() });
 	}
 
-	bmfile_ = std::make_unique<BMFontFile>(parseLines(lines));
-	mapped_textures_ = std::make_unique<std::unordered_map<int, MappedTexture>>();
+	bmfile_ = parseLines(lines);
+	mapped_textures_ = std::unordered_map<int, MappedTexture>();
 
-	texture_ = renderutil::loadTextureFromFile(sdl_renderer, "resources\\" + bmfile_->page.filename, &texture_rect_); // TODO: don't hardcode "resources\\", use same path as .fnt file
+	texture_ = renderutil::loadTextureFromFile(sdl_renderer, "resources\\" + bmfile_.page.filename, &texture_rect_); // TODO: don't hardcode "resources\\", use same path as .fnt file
 
-	for (auto& char_item : bmfile_->chars)
+	for (auto& char_item : bmfile_.chars)
 	{
 		auto index = char_item.first;
 		auto& item = char_item.second;
 		auto texture = MappedTexture{ texture_, SDL_Rect{ item.x, item.y, item.w, item.h } };
-		(*mapped_textures_)[index] = texture;
+		mapped_textures_[index] = texture;
 	}
 
 	debug::console({ "Loaded BMFont '", bmfont_path, "'" });
 }
 
-const std::unordered_map<int, MappedTexture>* bmfont::getMap()
+const std::unordered_map<int, MappedTexture>& bmfont::getMap()
 {
-	return mapped_textures_.get();
+	return mapped_textures_;
 }
 
-const bmfont::BMFontFile* bmfont::getDefinition()
+const bmfont::BMFontFile& bmfont::getDefinition()
 {
-	return bmfile_.get();
-}
-
-void bmfont::renderText(SDL_Renderer* sdl_renderer, const std::string& text, unsigned int x, unsigned int y)
-{
-	for (auto c : text)
-	{
-		auto font_texture = mapped_textures_->at(c);
-		SDL_Rect render_rect = { x + bmfile_->chars.at(c).xoffset, y + bmfile_->chars.at(c).yoffset, font_texture.rect.w, font_texture.rect.h };
-		SDL_RenderCopy(sdl_renderer, font_texture.texture, &font_texture.rect, &render_rect);
-		x += bmfile_->chars.at(c).xadvance;
-	}
+	return bmfile_;
 }
